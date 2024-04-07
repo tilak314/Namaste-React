939f04b66843c370e5635b165a1c5feedd935312
tilak314 <tilaktejasimhadri2001@gmail.com>
2024-04-07 09:47:17 +0530

    constant vals

---
+++ constants/mockData.js
@@ -0,0 +1,62 @@
+const resdata = [
+    {
+      id: "718294",
+      name: "Manjeera Family Restaurant",
+      cloudinaryImageId: "f907494ebedf7c5e186a09fe583fdb60",
+      locality: "Korramelu X Road",
+      areaName: "Uppal",
+      costForTwo: "168.03 only",
+      cuisines: ["Chicken pulao😎"],
+      avgRating: 300,
+    },
+    {
+      id: "112997",
+      name: "KFC",
+      cloudinaryImageId: "56c9ab92bd79745fd152a30fa2525426",
+      areaName: "Banjara Hills",
+      costForTwo: "₹150 for two",
+      cuisines: ["Wings", "Lollipops"],
+      avgRating: 4,
+    },
+    {
+      id: "700004",
+      name: "Hotel Tulips Grand",
+      cloudinaryImageId: "67db4cd57d4f2e7c1d092b8533850188",
+      locality: "Uppal",
+      areaName: "Pocharam",
+      costForTwo: "₹500 for two",
+      cuisines: ["Biryani", "South Indian"],
+      avgRating: 2
+    },
+    {
+      id: "157695",
+      name: "Varalakshmi Tiffins",
+      cloudinaryImageId: "8523266de1213e8ea57e5df4eedf747e",
+      areaName: "Dilsukhnagar",
+      costForTwo: "₹300 for two",
+      cuisines: ["South Indian"],
+      avgRating: 1.0,
+    },
+    {
+      id: "249260",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "urytpqsr126eqtnfwzvk",
+      locality: "Boduppal & Singapore township",
+      areaName: "Pocharam",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3,
+    },
+    {
+      id: "54276",
+      name: "Suruchi - The Family Restaurant",
+      cloudinaryImageId: "bjr9kwwaibqsyzdifwzu",
+      locality: "Narapalli Rd",
+      areaName: "Pocharam",
+      costForTwo: "₹350 for two",
+      cuisines: ["North Indian"],
+      avgRating: 4,
+    },
+  ];
+  export default resdata;
+  
