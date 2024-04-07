aaa3faf7664cf5365d4dc543ada9d46c4b1dd95a
tilak314 <tilaktejasimhadri2001@gmail.com>
2024-04-07 09:46:33 +0530

    created main App comp

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import './index.css';
+
+import Header from './components/Header';
+import Body from './components/Body';
+
+
+const App = () => {
+    return(
+        <div className='app'>
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
+
 const heading = React.createElement("h1", {}, "Hello World");;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(<App/>);
