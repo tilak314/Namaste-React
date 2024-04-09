2472bb361555508a106f02c5cb79c3c13ce80579
tilak314 <tilaktejasimhadri2001@gmail.com>
2024-04-09 17:40:40 +0530

    V6-Using Swiggy API

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,39 @@
 import Restaurent from "./Restaurent";
 import resdata from "../constants/mockData"; //default import
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listofrestaurents, setListofrestaurents] = useState(resdata);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    try {
+      const response = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4425254&lng=78.6479238&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await response.json();
+
+      async function checkJsonData(jsonData) {
+        for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+          let checkData =
+            json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle
+              ?.restaurants;
+
+          if (checkData !== undefined) {
+            return checkData;
+          }
+        }
+      }
+
+      const resData = await checkJsonData(json);
+      setAllRestaurants(resData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="body">
       <div className="search-cont">
@@ -14,15 +44,15 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            const list = listofrestaurents.filter((res) => res.avgRating > 4);
-            setListofrestaurents(list);
+            const list = allRestaurants.filter((res) => res.info.avgRating < 4);
+            setAllRestaurants(list);
           }}
         >
           Filter
         </button>
       </div>
       <div className="res-cont">
-        {listofrestaurents.map((res) => (
+        {allRestaurants.map((res) => (
           <Restaurent key={res.id} resdata={res} />
         ))}
       </div>
