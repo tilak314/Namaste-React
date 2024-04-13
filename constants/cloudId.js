122f5ffc465826f9435a53bc93fc51f10e9bc0be
tilak314 <tilaktejasimhadri2001@gmail.com>
2024-04-13 22:40:26 +0530

    V8-Added resManu constants

--- constants/cloudId.js
+++ constants/cloudId.js
@@ -4,3 +4,19 @@ export const cloudinaryImageIdURL =
 export const logoURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe4-aO_9Sh5b6Ja6DF-K5b1W3BH41Gozzs6w&s"
 //named export
 export const shimmer_card_unit = 13;
+export const shimmer_menu_card_unit = 4;
+
+
+export const swiggy_menu_api_URL ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.431062&lng=78.6398737&restaurantId="
+
+
+
+
+
+  export const MENU_ITEM_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+export const RESTAURANT_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.Restaurant";
+
+export const IMG_CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const ITEM_IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
